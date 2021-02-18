53652969efa63b98771f8fe8c311326e6bface5b
sflittrell <sflittrell@gmail.com>
2021-02-18 17:58:38 -0500

    added Blastoff! text

--- main.js
+++ main.js
@@ -2,46 +2,40 @@ const launchButton = document.getElementById('launchButton');
 
 launchButton.onclick = function() {
 
-    /*let userHour = document.getElementById('hours').value;
-    let userMin = document.getElementById('minutes').value;
-    let userSec = document.getElementById('seconds').value;*/
-
+    document.getElementById('Blastoff').innerHTML = ''
     let userDate = document.getElementById('launchDate').value;
     let userTime = document.getElementById('launchTime').value;
-    console.log(userDate);
-    console.log(userTime);
+    //console.log(userDate);
+    //console.log(userTime);
     let dateTime = userDate + 'T' + userTime;
-    console.log(dateTime);
+    //console.log(dateTime);
     const eqSec = 1000;
     const eqMin = eqSec * 60;
     const eqHour = eqMin * 60;
 
     const startTime = new Date().getTime();
-    console.log(startTime);
-    console.log(new Date());
-
-    /*const futureTimeTotal = startTime + ((userHour * eqHour) + (userMin * eqMin) + (userSec * eqSec));
-    console.log(futureTimeTotal);*/
+    //console.log(startTime);
+    //console.log(new Date());
 
     const futureTimeTotal = new Date(dateTime).getTime();
-    console.log(futureTimeTotal);
+    //console.log(futureTimeTotal);
 
-    setInterval(countdownTimer, 1000);
+    let pageRefresher = setInterval(countdownTimer, 1000);
 
     function countdownTimer() {
 
         let currentTime = new Date().getTime();
     
         let counterTime = futureTimeTotal - currentTime;
-        console.log(counterTime);
+        //console.log(counterTime);
 
         let countSec = Math.floor((counterTime % eqMin) / eqSec);
-        console.log(countSec);
+        //console.log(countSec);
         let countMin = Math.floor((counterTime % eqHour) / eqMin);
-        console.log(countMin);
+        //console.log(countMin);
         let countHour = Math.floor(counterTime / eqHour);
-        console.log(countHour);
-
+        //console.log(countHour);
+/*
         if (countSec < 10) {
             countSec = "0" + countSec;
         }
@@ -49,15 +43,16 @@ launchButton.onclick = function() {
             countSec = "0" + countMin;
         }
         if (countHour < 10) {
-            contHour = "0" + countHour;
+            countHour = "0" + countHour;
         }
-
+*/
         let displayTime = `${countHour}:${countMin}:${countSec}`;
-        console.log(displayTime);
+        //console.log(displayTime);
         document.getElementById('displayTime').innerHTML = displayTime;
 
-        if (counterTime = 0) {
-            clearInterval(countdownTimer);
+        if (counterTime <= 0) {
+            clearInterval(pageRefresher);
+            let displayTime = "00:00:00";
             document.getElementById('Blastoff').innerHTML = 'Blastoff!'
         }
     }
